d1c91497bdab34f51ccb5f804ffd16c1b26c2984
Yishak Kidanu <yisak04@gmail.com>
2022-10-16 14:28:11 +0000

    A flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,12 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 app.route('/', strict_slashes=False)
+
+
 def hello_HBNB():
     return "Hello HBNB!"
 
-app.route('/hbnb')
+app.route('/hbnb', strict_slashes=False)
+
 def display_hbnb():
-	return "HBNB"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
