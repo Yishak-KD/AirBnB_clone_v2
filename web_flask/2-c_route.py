157ac3c74f3406e50f9f45e2944a580caa596187
Yishak Kidanu <yisak04@gmail.com>
2022-10-16 20:20:09 +0000

    Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Flask web"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    new = text.replace("_", " ")
+    return "C " + new
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
